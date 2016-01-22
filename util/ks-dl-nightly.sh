810f61fdde4928acaa4afc9452b1e5cd56d09d64
Dino Morelli <dino@ui3.info>
2016-01-22 18:49:40 +0000

    Finalized production settings in this script

--- util/ks-dl-nightly.sh
+++ util/ks-dl-nightly.sh
@@ -1,24 +1,21 @@
 #! /bin/bash
 
-# Location of the ks-download binaries and scripts
-# Needed for the cron environment
-binDir=/opt/ks-download/bin
-#binDir=/home/dino/bin
+# Add the location of the ks-download binaries and scripts to the PATH
+PATH="/opt/ks-download/bin:${PATH}"
 
-configDir=/home/dino/.config/kitchensnitch
+configDir=/home/ksadmin/.config/ksnitch
 
-# Make this a directory that's backed-up
-workDirParent=/var/local/kitchensnitch/nc-wake_daily
-#workDirParent=/home/dino/dev/kitchensnitch/data/nc-wake_daily
+# This is where inspections are downloaded and places-matched
+# This is a good candidate for backup
+workDirParent=/data/ksnitch/download/nc_wake
+#workDirParent=/home/dino/dev/ksnitch/data/nc_wake_daily
 
 
 # When running outside of this zone (or on a system using UTC),
 # need this set explicitly for both the `date` binary and ks-dlinsp
 export TZ="America/New_York"
 
-workDir=${workDirParent}/nc-wake_$(date +"%Y-%m-%d" --date='2 days ago')
-
-PATH=$binDir:"${PATH}"
+workDir=${workDirParent}/nc_wake_$(date +"%Y-%m-%d" --date='2 days ago')
 
 
 mkdir -p $workDir/{insp,succ,fail}
@@ -46,11 +43,11 @@ ks-locate \
 
 # Import into MongoDB
 
-#ks-dbinsert $configDir succ/ > ks-dbinsert.log
+ks-dbinsert $configDir succ/ > ks-dbinsert.log
 
 # Use these to simulate import success or failure
 # comment these lines out if using the above ks-dbinsert command
-echo "Fake ks-dbinsert success" > ks-dbinsert.log; true
+#echo "Fake ks-dbinsert success" > ks-dbinsert.log; true
 #echo "Fake ks-dbinsert failure" > ks-dbinsert.log; false
 
 dbinsertExit=$?
