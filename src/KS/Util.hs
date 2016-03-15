9fd80f82f976f7b7e5879ae1f6384792f9cacab5
Dino Morelli <dino@ui3.info>
2016-03-15 20:54:06 +0000

    Added a function to compute if a date is in daylight savings time
    This function takes a Day data structure and figure out if it's in
    DST relative to the currently in-effect time zone.

--- src/KS/Util.hs
+++ src/KS/Util.hs
@@ -4,6 +4,7 @@
 module KS.Util
    ( withRetry
    , setDate
+   , isDST
    )
    where
 
@@ -11,6 +12,8 @@ import Control.Concurrent ( threadDelay )
 import Control.Monad.Catch ( catchAll )
 import Data.Time ( Day, TimeZone,
          addDays, getCurrentTime, localDay, utcToLocalTime )
+import Data.Time.Calendar ( toGregorian )
+import Data.Time.Calendar.WeekDate ( toWeekDate )
 
 
 withRetry :: Int -> Int -> IO a -> (String -> IO ()) -> IO (Either String a)
@@ -36,3 +39,36 @@ twoDaysAgo tz =
       localDay .                 -- ..extract the Day..
       utcToLocalTime tz) <$>     -- ..local time for the zone the source is in..
       getCurrentTime             -- This is UTC time..
+
+
+{- This computation is relative to the currently in-effect time zone
+   of the system. To make sure you have it right, it can be helpful
+   to set the environment variable yourself. Like this:
+
+   $ export TZ="America/New_York"
+-}
+isDST :: Day -> Bool
+isDST day =
+   -- January, February and December are out
+   if      (month < 3 || month > 11) then False
+
+   -- April to October are in
+   else if (month > 3 && month < 11) then True
+
+   -- In March, we are DST if our previous Sunday was on or after the 8th
+   else if (month == 3)              then previousSunday >= 8
+
+   -- In November we must be before the first Sunday to be DST
+   -- That means the previous Sunday must be before the 1st
+   else                                   previousSunday <= 0
+
+   where
+      (_, month, monthDay) = toGregorian day
+
+      -- Weekday in Haskell is Mo - Su, 1 - 7
+      -- but we need Su - Sa, 0 - 6
+      weekDay d = case toWeekDate d of
+         (_, _, 7 ) -> 0
+         (_, _, wd) -> wd
+
+      previousSunday = monthDay - (weekDay day)
