d634b4e5301f4aa251bf352326b22096091bd971
Dino Morelli <dino@ui3.info>
2016-04-30 14:42:40 +0000

    Added ability to specify installation directory
    Also added the --no-clean switch.

--- util/install.sh
+++ util/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-installDir="/opt/ksnitch"
+defaultInstallDir="/opt/ksnitch"
 sandboxDir="../cabal-sandbox"
 project="ks-download"
 version=$(perl -n -e '/^version: (.*)/ && print $1' ${project}.cabal)
@@ -15,11 +15,19 @@ function usage {
 $basename - Build and install $project
 
 usage:
-   $basename [OPTIONS]
+   $basename [OPTIONS] [DIR]
 
 options:
-   -l, --link  Create symlink in $installDir
-   -h, --help  This help info
+   -C, --no-clean  Do not do 'clean' build, see below
+   -l, --link      Create symlink in DIR
+   -h, --help      This help info
+
+DIR is the directory to install into. Defaults to $defaultInstallDir
+
+A 'clean' build will perform 'cabal clean', 'cabal sandbox init...', and 'cabal install --only-dep' before the build of this project. Not always necessary.
+
+Part of the Kitchen Snitch project
+2016-04-30  Dino Morelli <dino@ui3.info>
 
 USAGE
 }
@@ -27,18 +35,20 @@ USAGE
 
 # arg parsing
 
-getoptResults=$(getopt -o lh --long link,help -n $basename -- "$@")
+getoptResults=$(getopt -o Clh --long no-clean,link,help -n $basename -- "$@")
 
 if [ $? != 0 ]; then usage; exit 1; fi
 
 # Note the quotes around '$getoptResults': they are essential!
 eval set -- "$getoptResults"
 
+optClean=true
 optLink=false
 optHelp=false
 
 while true ; do
    case "$1" in
+      -C|--no-clean) optClean=false; shift;;
       -l|--link) optLink=true; shift;;
       -h|--help) optHelp=true; shift;;
       --) shift; break;;
@@ -47,10 +57,14 @@ done
 
 if $optHelp; then usage; exit 0; fi
 
+installDir=${1:-$defaultInstallDir}
 
-cabal clean
-cabal sandbox init --sandbox=$sandboxDir
-cabal install --only-dep
+if $optClean
+then
+   cabal clean
+   cabal sandbox init --sandbox=$sandboxDir
+   cabal install --only-dep
+fi
 cabal install --prefix=$installDir/${project}-${version} --datasubdir=.
 
 if $optLink
