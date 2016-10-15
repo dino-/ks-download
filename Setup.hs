58a57ba281544c6cb06ea7d7bf8ef220f01a19d4
Dino Morelli <dino@ui3.info>
2016-10-15 17:07:51 +0000

    Switched to hsinstall for installation

--- Setup.hs
+++ Setup.hs
@@ -1,53 +1,7 @@
-#! /usr/bin/env runhaskell
+module Main (main) where
 
--- License: BSD3 (see LICENSE)
--- Author: Dino Morelli <dino@ui3.info>
+import Distribution.Simple
 
-import Control.Monad ( forM_ )
-import Distribution.Simple ( defaultMainWithHooks, postCopy,
-   simpleUserHooks )
-import Distribution.Simple.LocalBuildInfo
-import Distribution.Simple.Setup ( CopyFlags (..), Flag (..) )
-import System.Directory ( copyFile )
-import System.FilePath
+main :: IO ()
+main = defaultMain
 
-
-copyBinDestDir :: CopyFlags -> LocalBuildInfo -> FilePath
-copyBinDestDir copyFlags localBuildInfo = destDir
-   where
-      copyPrefix = case copyDest copyFlags of
-         Flag NoCopyDest  -> ""
-         Flag (CopyTo cp) -> cp ++ "/"
-         {- Can't use </> here because instPrefix is often absolute
-            and </> returns the second path if ALONE if it's absolute.
-            Safer to just have the extra / if that's how it goes.
-         -}
-
-      instPrefix = fromPathTemplate . prefix
-         . installDirTemplates $ localBuildInfo
-
-      destDir = copyPrefix ++ instPrefix </> "bin"
-
-
-copyCustomBinFiles :: FilePath -> IO ()
-copyCustomBinFiles destDir = do
-   -- src path info
-   let srcDir = "util"
-   let srcFiles =
-         [ "ks-dl-nightly-cdp.sh"
-         , "ks-dl-nightly-dhd.sh"
-         ]
-
-   putStrLn $ "Installing additional binaries in " ++ destDir
-
-   forM_ srcFiles $ \srcFile ->
-      copyFile (srcDir </> srcFile) (destDir </> srcFile)
-
-
-main = defaultMainWithHooks (simpleUserHooks
-   { postCopy = customPostCopy
-   } )
-
-   where
-      customPostCopy _ copyFlags _ localBuildInfo =
-         copyCustomBinFiles (copyBinDestDir copyFlags localBuildInfo)
