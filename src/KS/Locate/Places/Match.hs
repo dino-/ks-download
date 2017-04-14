0bf68e25b8576f9728a6f44b21fb9340438ace21
Dino Morelli <dino@ui3.info>
2017-04-14 01:15:52 +0000

    Fixed bugs with address matching
    This was a recent fix that proved to be broken in development
    testing. Not only is it fixed but we now have unit tests for
    these cases.

--- src/KS/Locate/Places/Match.hs
+++ src/KS/Locate/Places/Match.hs
@@ -5,7 +5,9 @@
 {-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 
 module KS.Locate.Places.Match
-   ( Match, match )
+   ( Match, match
+   , test_cleanAddress
+   )
    where
 
 import Data.Attoparsec.Text hiding ( count, match )
@@ -15,6 +17,7 @@ import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Format as TF
 import Prelude hiding ( takeWhile )
+import Test.Hspec
 
 import KS.Locate.Locate
 import qualified KS.Data.Inspection as I
@@ -91,8 +94,10 @@ isMatch iaddr pvic =
    where
       pIaddr = prefix iaddr
       pNewPvic = prefix newPvic
-      newPvic = removePrefixZip pvic
+      newPvic = cleanAddress pvic
       prefix = T.takeWhile isDigit
+
+
 
 
 {- We get these ridiculous addresses from Google Places where they've
@@ -102,9 +107,29 @@ isMatch iaddr pvic =
       "27603, 7900 Fayetteville Road, Raleigh"
       "Wells Fargo Capital Center, 150 Fayetteville St, Raleigh"
 
-   This parser returns the address string with this garbage removed, up 
-   to the street address number.
+   This group of functions returns the address string with this
+   garbage removed, up to the street address number.
+
+   What we want:
+
+   - If the string doesn't start with a number
+      - Remove everything up to and including the first occurrance of ", "
+   - If the string is a number
+      - removePrefixZip function (i.e. digits up to and including ", ")
 -}
+
+cleanAddress :: T.Text -> T.Text
+cleanAddress pvic
+   -- The address is empty! Ugh but just send it back.
+   | T.null pvic = pvic
+
+   -- Starts with a number, send to the pre-zip-code remover
+   | isDigit $ T.head pvic = removePrefixZip pvic
+
+   -- No number, send to the thing that tries to remove the building name or whatever they did at Google
+   | otherwise = removeFirstLine pvic
+
+
 removePrefixZip :: T.Text -> T.Text
 removePrefixZip =
    either T.pack id . parseOnly (choice [prefixZip, everythingElse])
@@ -115,5 +140,32 @@ removePrefixZip =
 
       prefixZip :: Parser T.Text
       prefixZip = do
+         try $ manyTill digit $ string ", "
+         everythingElse
+
+
+removeFirstLine :: T.Text -> T.Text
+removeFirstLine =
+   either T.pack id . parseOnly (choice [prefixZip, everythingElse])
+
+   where
+      everythingElse :: Parser T.Text
+      everythingElse = takeWhile $ const True
+
+      prefixZip :: Parser T.Text
+      prefixZip = do
          try $ manyTill anyChar $ string ", "
          everythingElse
+
+
+test_cleanAddress :: SpecWith ()
+test_cleanAddress = describe "cleanAddress" $ do
+   it "conventional address" $
+      cleanAddress "4035 Lake Boone Trail #109, Raleigh"
+      `shouldBe` "4035 Lake Boone Trail #109, Raleigh"
+   it "building before street address" $
+      cleanAddress "Wells Fargo Capitol Center, 150 Fayetteville Street #2800, Raleigh"
+      `shouldBe` "150 Fayetteville Street #2800, Raleigh"
+   it "prefixed by zip code" $
+      cleanAddress "27604, 3501 Capital Boulevard, Raleigh"
+      `shouldBe` "3501 Capital Boulevard, Raleigh"
--- testsuite/test-Locate.hs
+++ testsuite/test-Locate.hs
@@ -4,8 +4,10 @@
 import Test.Hspec
 
 import qualified NameWords
+import KS.Locate.Places.Match ( test_cleanAddress )
 
 
 main :: IO ()
 main = hspec $ do
    NameWords.tests
+   test_cleanAddress
