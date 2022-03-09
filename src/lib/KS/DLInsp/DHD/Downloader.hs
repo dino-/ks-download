925d873278f579954f95953dcbf83be86220ee1e
Dino Morelli <dino@ui3.info>
2022-03-09 15:37:44 -0500

    Fixed broken Wake county inspection gathering
    The problem was that Wake switched their system over to https but we
    were still using http. The fix required that we replace the (old) HTTP
    library in Haskell that doesn't support https at all and instead use
    wreq.
    
    Closes trello board Kitchen Snitch card #264

--- src/lib/KS/DLInsp/DHD/Downloader.hs
+++ src/lib/KS/DLInsp/DHD/Downloader.hs
@@ -1,6 +1,7 @@
 -- License: BSD3 (see LICENSE)
 -- Author: Dino Morelli <dino@ui3.info>
 
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
 module KS.DLInsp.DHD.Downloader
@@ -8,17 +9,20 @@ module KS.DLInsp.DHD.Downloader
    where
 
 import Control.Concurrent ( threadDelay )
+import Control.Lens ( (^.) )
 import Control.Monad ( (>=>) )
+import Data.ByteString.Lazy ( ByteString )
 import           Data.Either ( partitionEithers )
-import           Data.List ( intercalate, isInfixOf, isPrefixOf )
+import           Data.List ( isInfixOf, isPrefixOf )
 import           Data.Maybe ( fromJust, fromMaybe )
+import Data.Monoid ( (<>) )
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TL
 import           Data.Time.Calendar ( toGregorian )
 -- import           Debug.Trace ( trace )
 import qualified KS.Data.Inspection as I
-import           Network.HTTP ( HStream, Request, Request_String,
-                  getResponseBody, getRequest, postRequestWithBody,
-                  simpleHTTP, urlEncode )
+import Network.Wreq ( FormParam ((:=)), Response, get, post, responseBody )
 import           Text.HTML.TagSoup ( Tag (TagText), (~==), (~/=), fromAttrib,
                   fromTagText, innerText, isTagClose, parseTags, partitions,
                   sections )
@@ -31,7 +35,7 @@ import           KS.Util ( withRetry )
 
 
 urlPrefix :: String
-urlPrefix = "http://wake-nc.healthinspections.us/"
+urlPrefix = "https://wake-nc.healthinspections.us/"
 
 
 inspectionSrc :: String
@@ -53,6 +57,10 @@ download options destDir = runDL options $ do
       mapM_ (I.saveInspection destDir >=> printf "Saved inspection file %s\n")) getters
 
 
+reqToTags :: IO (Response ByteString) -> IO [Tag String]
+reqToTags = fmap (parseTags . TL.unpack . TL.decodeUtf8 . (^. responseBody))
+
+
 -- Get all (4) facilities from a page at the supplied URL
 getFacilities :: String -> IO [I.Inspection]
 getFacilities url = do
@@ -62,7 +70,7 @@ getFacilities url = do
    printf "Retrieving %s\n" url
 
    tags <- either error return =<< withRetry 5 2
-      (parseTags `fmap` (openURL . getRequest $ urlPrefix ++ url)) putStrLn
+      (reqToTags (get $ urlPrefix <> url)) putStrLn
 
    let itags = isolateInspTags tags
    putStrLn $ "Facilities on this page (should never be 0): " ++ (show . length $ itags)  -- FIXME
@@ -75,7 +83,7 @@ getFacilities url = do
 isolateInspTags :: [Tag String] -> [[Tag String]]
 isolateInspTags = partitions isFacAnchor
    where isFacAnchor e =
-            (e ~== "<a href>") &&
+            (e ~== ("<a href>" :: String)) &&
             (isPrefixOf "facilities" $ fromAttrib "href" e) &&
             (not . elem '&' $ fromAttrib "href" e)
 
@@ -97,10 +105,10 @@ extractInsp tags = return $ makeInspection <$> I.parseDate date
          (urlPrefix ++ detail)
 
       name = innerText . (takeWhile (not . isTagClose)) $ tags
-      TagText addr = (dropWhile (~/= "Location:") tags) !! 2
-      TagText date = (dropWhile (~/= "Inspection Date:") tags) !! 2
-      TagText score = (dropWhile (~/= "Score:") tags) !! 2
-      detailTag = head . (dropWhile (~/= "<input type=button>")) $ tags
+      TagText addr = (dropWhile (~/= ("Location:" :: String)) tags) !! 2
+      TagText date = (dropWhile (~/= ("Inspection Date:" :: String)) tags) !! 2
+      TagText score = (dropWhile (~/= ("Score:" :: String)) tags) !! 2
+      detailTag = head . (dropWhile (~/= ("<input type=button>" :: String))) $ tags
       reinspection = fromAttrib "value" detailTag
       detail = extractDetailUrl . fromAttrib "onclick" $ detailTag
 
@@ -116,17 +124,17 @@ extractInsp tags = return $ makeInspection <$> I.parseDate date
       -- This code is for extracting the violations data. Tricky!
 
       -- Table containing the violations
-      vtable = (dropWhile (~/= "<table cellpadding=2>")) $ tags
+      vtable = (dropWhile (~/= ("<table cellpadding=2>" :: String))) $ tags
 
       -- Each violations table row. Each of these has 3 tdS
-      allRowsInViolsTable = partitions (~== "<tr>") vtable
+      allRowsInViolsTable = partitions (~== ("<tr>" :: String)) vtable
 
       -- Discard the first row, it's display labels
       allRowsMinusHeader = tail $ allRowsInViolsTable
 
       -- Discard the rows after the last violation
       vrows = takeWhile isViol allRowsMinusHeader
-      isViol tags' = length (filter (~== "General Comments") tags') == 0
+      isViol tags' = length (filter (~== ("General Comments" :: String)) tags') == 0
 
       -- Extract the violations and critical-ness from the remaining tags
       vs = map extractViolation vrows
@@ -151,15 +159,17 @@ extractViolation tags = (crit, text)
 getPageUrls :: DL [String]
 getPageUrls = do
    liftIO $ putStrLn "\nRetrieving all page URLs"
-   post <- mkPost
-   dlResult <- liftIO $ withRetry 5 2 (parseTags `fmap` openURL post) putStrLn
-   tags <- either error return dlResult
+   formParams <- searchParams
+   etags <- liftIO $ withRetry 5 2
+      (reqToTags (post (urlPrefix ++ "reports.cfm") formParams))
+      putStrLn
+   tags <- either error return etags
    return $ map (\i -> subRegex (mkRegex " ") i "%20") . map (fromAttrib "href" . head) .
-      sections (~== "<a class=teaser>") $ tags
+      sections (~== ("<a class=teaser>" :: String)) $ tags
 
 
--- Used for debugging to store the search results page locally
--- for inspection
+-- Used (in the distant past) for debugging to store the search results page
+-- locally for inspection
 {- FIXME
 savePage :: IO ()
 savePage = do
@@ -168,24 +178,16 @@ savePage = do
 -}
 
 
--- Retrieve a page
-openURL :: HStream b => Request b -> IO b
-openURL p = getResponseBody =<< simpleHTTP p
-
-
 {- These things are for the first form post to get all of the page URLs
 -}
 
-mkPost :: DL Request_String
-mkPost = do
-   ps <- searchParams
-   return $ postRequestWithBody
-      (urlPrefix ++ "reports.cfm")
-      "application/x-www-form-urlencoded"
-      $ intercalate "&" ps
+
+fvAny, fvEmpty :: T.Text
+fvAny = "Any"
+fvEmpty = ""
 
 
-searchParams :: DL [String]
+searchParams :: DL [FormParam]
 searchParams = do
    (sy, sm, sd) <- toGregorian . fromJust <$> asks optStartDate
    (ey, em, ed) <- toGregorian . fromJust <$> asks optEndDate
@@ -194,23 +196,23 @@ searchParams = do
    liftIO $ printf "Retrieving inspections for establishment type: %s\n" estType
 
    return $
-      [ "f=search"
-      , "strSearch1="
-      , "relevance1=fName"
-      , "strSearch2="
-      , "relevance2=fName"
-      , "strSearch3="
-      , "relevance3=fName"
-      , "lscore="
-      , "hscore="
-      , "ftype=" ++ (urlEncode estType)
-      , "fzipcode=Any"
-      , "rcritical=Any"
-      , "sMonth=" ++ (show sm)
-      , "sDay=" ++ (show sd)
-      , "sYear=" ++ (show sy)
-      , "eMonth=" ++ (show em)
-      , "eDay=" ++ (show ed)
-      , "eYear=" ++ (show ey)
-      , "func=Search"
+      [ "f" := ("search" :: T.Text)
+      , "strSearch1" := fvEmpty
+      , "relevance1" := ("fName" :: T.Text)
+      , "strSearch2" := fvEmpty
+      , "relevance2" := ("fName" :: T.Text)
+      , "strSearch3" := fvEmpty
+      , "relevance3" := ("fName" :: T.Text)
+      , "lscore" := fvEmpty
+      , "hscore" := fvEmpty
+      , "ftype" := T.pack estType
+      , "fzipcode" := fvAny
+      , "rcritical" := fvAny
+      , "sMonth" := sm
+      , "sDay" := sd
+      , "sYear" := sy
+      , "eMonth" := em
+      , "eDay" := ed
+      , "eYear" := ey
+      , "func" := ("Search" :: T.Text)
       ]
